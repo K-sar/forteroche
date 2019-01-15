e133bb599d90ec73a4ab1ca70fb2367f39cfbad9
K-sar <muller.clement@laposte.net>
2019-01-15 21:01:29 +0100

    debugage (j'aurais du faire une branche je sais)

--- lib/vendor/Model/ChaptersManager.php
+++ lib/vendor/Model/ChaptersManager.php
@@ -28,7 +28,7 @@ abstract class ChaptersManager extends Manager
     }
     else
     {
-      throw new \RuntimeException('Le chapitre doit être validée pour être enregistrée');
+      throw new \RuntimeException('Le chapitre doit être validé pour être enregistré');
     }
   }
   
