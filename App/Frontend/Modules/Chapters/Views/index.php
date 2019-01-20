1b075ca24dde308cf82298ff7bc203660637e234
K-sar <muller.clement@laposte.net>
2019-01-20 18:49:40 +0100

    L'accueil identifié permet de modifier et supprimer directement

--- App/Frontend/Modules/Chapters/Views/index.php
+++ App/Frontend/Modules/Chapters/Views/index.php
@@ -4,5 +4,9 @@ foreach ($listeChapters as $chapters)
 ?>
   <h2><a href="chapters-<?= $chapters['id'] ?>.html"><?= $chapters['titre'] ?></a></h2>
   <p><?= nl2br($chapters['contenu']) ?></p>
+  <?php if ($user->isAuthenticated()) { ?> -
+    <a href="admin/chapters-update-<?= $chapters['id'] ?>.html">Modifier</a> |
+    <a href="admin/chapters-delete-<?= $chapters['id'] ?>.html">Supprimer</a>
+  <?php } ?>
 <?php
 }
