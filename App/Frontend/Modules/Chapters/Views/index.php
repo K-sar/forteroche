01740a69a06b6202249c605c952673d1af909f15
K-sar <muller.clement@laposte.net>
2019-02-03 19:04:07 +0100

    bouton "caché" dans l'index frontend

--- App/Frontend/Modules/Chapters/Views/index.php
+++ App/Frontend/Modules/Chapters/Views/index.php
@@ -5,9 +5,10 @@ foreach ($listeChapters as $chapters)
   <h2><a href="chapters-<?= $chapters['id'] ?>.html">Chapitre <?= $chapters['chapitre'] ?> : <?= $chapters['titre'] ?></a></h2>
   <p><?= nl2br($chapters['contenu']) ?></p>
   <div class="btn-group" role="group" aria-label="Basic example">
-    <?php if ($user->isAuthenticated()) { ?>
-    <button type="button" class="btn btn-secondary"><a href="admin/chapters-update-<?= $chapters['id'] ?>.html" style="text-decoration:none; color:black">Modifier</a></button>
-    <button type="button" class="btn btn-secondary"><a href="admin/chapters-delete-<?= $chapters['id'] ?>.html" style="text-decoration:none; color:black">Supprimer</a></button>
+    <?php if ($user->isAuthenticated()) { ?>            
+    <a href="admin/chapters-publish-<?= $chapters['id'] ?>.html" class="btn btn-secondary">Cacher</a>
+    <a href="admin/chapters-update-<?= $chapters['id'] ?>.html" class="btn btn-secondary">Modifier</a>
+    <a href="admin/chapters-delete-<?= $chapters['id'] ?>.html" class="btn btn-secondary">Supprimer</a>
     <?php } ?>  
     </div>
 <?php
