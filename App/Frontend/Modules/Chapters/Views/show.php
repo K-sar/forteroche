0413c7449d95e654f523c57a11ab50972279f941
K-sar <muller.clement@laposte.net>
2019-02-03 19:00:26 +0100

    "numéro" de chapitre et boutons dans show

--- App/Frontend/Modules/Chapters/Views/show.php
+++ App/Frontend/Modules/Chapters/Views/show.php
@@ -1,9 +1,21 @@
+<h2><?php if (!preg_match('#[eé]pilogue#i', $chapters['complement'])) 
+  {
+    echo( 'Chapitre '. $chapters['chapitre']. ' ');
+  }
+  echo($chapters['complement']) ?></h2>
+
 <h2><?= $chapters['titre'] ?></h2>
 <p><?= nl2br($chapters['contenu']) ?></p>
 <p>Par <em><?= $chapters['auteur'] ?></em>, le <?= $chapters['dateAjout']->format('d/m/Y à H\hi') ?></p>
 <?php if ($chapters['dateAjout'] != $chapters['dateModif']) { ?>
   <p style="text-align: right;"><small><em>Modifiée le <?= $chapters['dateModif']->format('d/m/Y à H\hi') ?></em></small></p>
 <?php } ?>
+
+<div class="btn-group" role="group" aria-label="Basic example">            
+  <a href="admin/chapters-publish-<?= $chapters['id'] ?>.html" class="btn btn-secondary"><?php if($chapters['publication'] == 0){echo('Publier');}else{echo('Cacher');} ?></a>
+  <a href="admin/chapters-update-<?= $chapters['id'] ?>.html" class="btn btn-secondary">Modifier</a>
+  <a href="admin/chapters-delete-<?= $chapters['id'] ?>.html" class="btn btn-secondary">Supprimer</a>
+</div>
 
 <p><a href="commenter-<?= $chapters['id'] ?>.html">Ajouter un commentaire</a></p>
 
